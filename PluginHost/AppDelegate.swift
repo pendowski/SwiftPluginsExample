53fe09682e290f0d0d52748b64f00c04dc07d284
Jarosław Pendowski <jarek@pendowski.com>
2016-04-02 14:30:07 +0200

    Initial Commit

---
+++ PluginHost/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PluginHost
+//
+//  Created by Jarosław Pendowski on 02.04.2016.
+//  Copyright © 2016 Jarosław Pendowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
